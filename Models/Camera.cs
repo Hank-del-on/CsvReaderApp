57698ae2a25242c95f9304d8e8dbeee0dd75681b
Hank-del-on <hankhankkodehode@gmail.com>
2025-03-06 12:18:46 +0100

    Canera cvs database

---
+++ Models/Camera.cs
@@ -0,0 +1,9 @@
+namespace CsvReaderApp
+{
+    public class Camera
+    {
+        public int Id { get; set; }          
+        public string Model { get; set; }
+        public string Description { get; set; }
+    }
+}
